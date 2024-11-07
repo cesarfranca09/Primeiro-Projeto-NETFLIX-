6c2127f634722c33d9d490407c5864e0b85f3f65
cesarfranca09 <cesarfranca.09@gmail.com>
2024-11-07 11:06:19 -0300

    Primeiro Commit - Projeto Netflix

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let botaosom = document.querySelector (".som")
+let video = document.querySelector (".video")
+
+botaosom.addEventListener ("click", ligasom,)
+
+function ligasom (){
+    video.muted = false
+}
